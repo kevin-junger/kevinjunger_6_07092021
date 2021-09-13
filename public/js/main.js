718db6b46b4642308fde9f8b532df1347e166d7f
Kevin Junger <kevin.junger@outlook.fr>
2021-09-13 16:55:39 +0200

    fetchProfiles() V2

--- public/js/main.js
+++ public/js/main.js
@@ -4,30 +4,40 @@ function fetchProfiles() {
       return response.json();
     })
     .then((data) => {
-      let html = "";
+      const container = document.querySelector(".profiles");
       for (i = 0; i < data.photographers.length; i++) {
-        html += `<figure class="profile">
-          <img class="profile__pic pp--big"
+        container.insertAdjacentHTML(
+          "beforeend",
+          `<figure class="profile" id="${data.photographers[i].id}"></figure>`
+        );
+        const figure = document.getElementById(data.photographers[i].id);
+        figure.insertAdjacentHTML(
+          "beforeend",
+          `<img class="profile__pic pp--big"
             src="content/photographers/${data.photographers[i].portrait}"
             alt="${data.photographers[i].name}"
           />
-          <figcaption class="profile__caption">
-            <h2 class="profile__name">${data.photographers[i].name}</h2>
-            <h3 class="profile__location">${data.photographers[i].city}, ${data.photographers[i].country}</h3>
-            <blockquote class="profile__quote">
-              ${data.photographers[i].tagline}
-            </blockquote>
-            <h4 class="profile__price">${data.photographers[i].price}€/jour</h4>
-            <ul class="profile__tags tags">`;
-        let tags = data.photographers[i].tags
-          .map((tag) => `<li class="profile__tag tags__tag">#${tag}</li>`)
-          .join("");
-        html += `${tags}
-            </ul>
-          </figcaption>
-        </figure>`;
+          <figcaption class="profile__caption"></figcaption>`
+        );
+        const figcaption = figure.querySelector(".profile__caption");
+        figcaption.insertAdjacentHTML(
+          "beforeend",
+          `<h2 class="profile__name">${data.photographers[i].name}</h2>
+          <h3 class="profile__location">${data.photographers[i].city}, ${data.photographers[i].country}</h3>
+          <blockquote class="profile__quote">
+            ${data.photographers[i].tagline}
+          </blockquote>
+          <h4 class="profile__price">${data.photographers[i].price}€/jour</h4>
+          <ul class="profile__tags tags"></ul>`
+        );
+        const tags = figcaption.querySelector(".profile__tags");
+        tags.insertAdjacentHTML(
+          "beforeend",
+          data.photographers[i].tags
+            .map((tag) => `<li class="profile__tag tags__tag">#${tag}</li>`)
+            .join("")
+        );
       }
-      document.querySelector(".profiles").innerHTML = html;
     });
 }
 function fetchProfile() {
