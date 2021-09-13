8051d19e73f6a032cce116f321b197c8309c5e37
Kevin Junger <kevin.junger@outlook.fr>
2021-09-13 14:07:02 +0200

    another fetch test

--- public/js/main.js
+++ public/js/main.js
@@ -5,8 +5,25 @@ function fetchData() {
     })
     .then((data) => {
       console.log(data.photographers);
-      document.getElementById('test-nom').innerText = `${data.photographers[0].name}`;
-      console.log(data.media);
+      document.getElementById('test').innerHTML = 
+        `<img class="profile__pic pp--big"
+          src="content/photographers/${data.photographers[0].portrait}"
+          alt="${data.photographers[0].name}"
+        />
+        <figcaption class="profile__caption">
+          <h2 class="profile__name">${data.photographers[0].name}</h2>
+          <h3 class="profile__location">${data.photographers[0].city}, ${data.photographers[0].country}</h3>
+          <blockquote class="profile__quote">
+            ${data.photographers[0].tagline}
+          </blockquote>
+          <h4 class="profile__price">${data.photographers[0].price}€/jour</h4>
+          <ul class="profile__tags tags">
+            <li class="tags__tag">#${data.photographers[0].tags[0]}</li>
+            <li class="tags__tag">#${data.photographers[0].tags[1]}</li>
+            <li class="tags__tag">#${data.photographers[0].tags[2]}</li>
+            <li class="tags__tag">#${data.photographers[0].tags[3]}</li>
+          </ul>
+        </figcaption>`;
     });
 }
 fetchData();
