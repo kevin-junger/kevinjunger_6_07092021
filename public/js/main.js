28226104e3e748ed6485e1729d610c7786a2ddef
Kevin Junger <kevin.junger@outlook.fr>
2021-09-13 16:25:32 +0200

    fetch individual profile info (very basic)

--- public/js/main.js
+++ public/js/main.js
@@ -5,9 +5,8 @@ function fetchProfiles() {
     })
     .then((data) => {
       let html = "";
-      for(i = 0; i < data.photographers.length; i++){
-        html +=
-        `<figure class="profile">
+      for (i = 0; i < data.photographers.length; i++) {
+        html += `<figure class="profile">
           <img class="profile__pic pp--big"
             src="content/photographers/${data.photographers[i].portrait}"
             alt="${data.photographers[i].name}"
@@ -20,7 +19,9 @@ function fetchProfiles() {
             </blockquote>
             <h4 class="profile__price">${data.photographers[i].price}€/jour</h4>
             <ul class="profile__tags tags">`;
-        let tags = data.photographers[i].tags.map(tag => `<li class="tags__tag">#${tag}</li>`).join("");
+        let tags = data.photographers[i].tags
+          .map((tag) => `<li class="profile__tag tags__tag">#${tag}</li>`)
+          .join("");
         html += `${tags}
             </ul>
           </figcaption>
@@ -29,4 +30,37 @@ function fetchProfiles() {
       document.querySelector(".profiles").innerHTML = html;
     });
 }
+function fetchProfile() {
+  fetch("../../FishEyeData.json")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      let html = "";
+      for (i = 0; i < data.photographers.length; i++) {
+        if (data.photographers[i].id === 243) {
+          html += `<img
+              class="about__pic pp"
+              src="content/photographers/${data.photographers[i].portrait}"
+              alt="${data.photographers[i].name}"
+            />
+            <h2 class="about__name">${data.photographers[i].name}</h2>
+            <h3 class="about__location">${data.photographers[i].city}, ${data.photographers[i].country}</h3>
+            <blockquote class="about__quote">
+              ${data.photographers[i].tagline}
+            </blockquote>`;
+          let tags = data.photographers[i].tags
+            .map((tag) => `<li class="about__tag tags__tag">#${tag}</li>`)
+            .join("");
+          html += `${tags}
+          </ul>
+          <button type="button" class="about__cta cta">
+            Contactez-moi
+          </button>`;
+        }
+      }
+      document.querySelector(".about").innerHTML = html;
+    });
+}
 fetchProfiles();
+fetchProfile();
