19e9d44d68377368c3a64c94548efe9e1cb29e6c
Kevin Junger <kevin.junger@outlook.fr>
2021-10-05 15:52:05 +0200

    💩 form validation testing

--- app/View/Elements/Form.js
+++ app/View/Elements/Form.js
@@ -1,8 +1,6 @@
 export default class Form {
   constructor() {
-    this.names = document.querySelectorAll('.form__input[type="text"]')
-    this.email = document.querySelector('.form__input[type="email"]')
-    this.message = document.querySelector('.form_message')
+    this.inputs = document.querySelectorAll('.form__input')
     this.submit = document.querySelector('.form__submit')
     this.regexAlpha = /^[a-zA-ZÀ-ÖØ-öø-ÿ- \s]+$/
     this.regexEmail =
@@ -11,45 +9,50 @@ export default class Form {
 
   init() {
     this.submit.addEventListener('click', (e) => {
-      if (!this.checkName() || !this.checkEmail() || !this.checkMessage()) {
-        window.alert(
-          'Une ou plusieurs informations sont manquantes ou incorrectes.'
-        )
-        e.preventDefault()
-      } else {
-        window.alert('OK')
+      if (
+        !this.checkFirstName() ||
+        !this.checkLastName() ||
+        !this.checkEmail()
+      ) {
+        console.log('prout')
         e.preventDefault()
       }
     })
   }
 
-  checkName() {
-    this.names.forEach((name) => {
-      if (
-        name.value.trim() === '' ||
-        name.value.trim().length < 2 ||
-        this.regexAlpha.test(name.value.trim()) === false
-      ) {
-        return false
-      }
-      return true
-    })
+  checkFirstName() {
+    if (
+      this.inputs[0].value.trim() === '' ||
+      !this.regexAlpha.test(this.inputs[0].value.trim())
+    ) {
+      console.log(false)
+      return false
+    }
+    console.log(true)
+    return true
+  }
+
+  checkLastName() {
+    if (
+      this.inputs[1].value.trim() === '' ||
+      !this.regexAlpha.test(this.inputs[1].value.trim())
+    ) {
+      console.log(false)
+      return false
+    }
+    console.log(true)
+    return true
   }
 
   checkEmail() {
     if (
-      this.email.value.trim() === '' ||
-      this.regexEmail.test(this.email.value.trim()) === false
+      this.inputs[2].value.trim() === '' ||
+      !this.regexEmail.test(this.inputs[2].value.trim())
     ) {
+      console.log(false)
       return false
     }
-    return true
-  }
-
-  checkMessage() {
-    if (this.message.value.trim() === '') {
-      return false
-    }
+    console.log(true)
     return true
   }
 }
