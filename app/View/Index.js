455b03f42ee2b062c6c38edfd8a673893123f8f0
Kevin Junger <kevin.junger@outlook.fr>
2021-10-17 21:02:57 +0200

    ♻️ refactor: View/Index

--- app/View/Index.js
+++ app/View/Index.js
@@ -14,14 +14,15 @@ export default class Index {
       'animals',
       'events',
     ]
-    this.profiles = []
-    photographers.forEach((element) => {
-      this.profiles.push(new Photographer(element))
+    this.photographers = []
+    photographers.forEach((photographer) => {
+      this.photographers.push(new Photographer(photographer))
     })
   }
 
   init(tag) {
     this.displayNavbar()
+    this.mainContainer.classList.add('index')
     if (!tag) {
       this.displayIndex()
     } else {
@@ -46,61 +47,40 @@ export default class Index {
   }
 
   displayIndex() {
-    this.mainContainer.classList.add('index')
-    this.profiles.forEach((element) => {
-      const card = document.createElement('figure')
-      card.className = 'photographer'
-      const html = `
-        <a class="photographer__link" href="?profile=${element.getId()}">
-          <img class="photographer__pic" src="public/content/photographers/${element.getPortrait()}" />
-          <h2 class="photographer__name">${element.getName()}</h2>
-        </a>
-        <h3 class="photographer__location">${element.getLocation()}</h3>
-        <blockquote class="photographer__quote">${element.getTagline()}</blockquote>
-        <h4 class="photographer__price">${element.getPrice()}</h4>
-        <ul class="photographer__tags tags">
-          ${element
-            .getTags()
-            .map(
-              (tag) => `<li class="photographer__tag tags__tag">#${tag}</li>`
-            )
-            .join(' ')}
-        </ul>
-      `
-      card.innerHTML = html
-      this.mainContainer.appendChild(card)
+    this.photographers.forEach((photographer) => {
+      this.displayPhotographer(photographer)
     })
   }
 
   displayIndexByTag(tag) {
-    this.mainContainer.classList.add('index')
-    this.profiles.forEach((element) => {
-      element.tags.forEach((e) => {
+    this.photographers.forEach((photographer) => {
+      photographer.tags.forEach((e) => {
         if (e === tag) {
-          const card = document.createElement('figure')
-          card.className = 'photographer'
-          const html = `
-            <a class="photographer__link" href="?profile=${element.getId()}">
-              <img class="photographer__pic" src="public/content/photographers/${element.getPortrait()}" />
-              <h2 class="photographer__name">${element.getName()}</h2>
-            </a>
-            <h3 class="photographer__location">${element.getLocation()}</h3>
-            <blockquote class="photographer__quote">${element.getTagline()}</blockquote>
-            <h4 class="photographer__price">${element.getPrice()}</h4>
-            <ul class="photographer__tags tags">
-              ${element
-                .getTags()
-                .map(
-                  (tag) =>
-                    `<li class="photographer__tag tags__tag">#${tag}</li>`
-                )
-                .join(' ')}
-            </ul>
-          `
-          card.innerHTML = html
-          this.mainContainer.appendChild(card)
+          this.displayPhotographer(photographer)
         }
       })
     })
+  }
+
+  displayPhotographer(data) {
+    const card = document.createElement('figure')
+    card.className = 'photographer'
+    const html = `
+      <a class="photographer__link" href="?profile=${data.getId()}">
+        <img class="photographer__pic" src="public/content/photographers/${data.getPortrait()}" />
+        <h2 class="photographer__name">${data.getName()}</h2>
+      </a>
+      <h3 class="photographer__location">${data.getLocation()}</h3>
+      <blockquote class="photographer__quote">${data.getTagline()}</blockquote>
+      <h4 class="photographer__price">${data.getPrice()}</h4>
+      <ul class="photographer__tags tags">
+        ${data
+          .getTags()
+          .map((tag) => `<li class="photographer__tag tags__tag">#${tag}</li>`)
+          .join(' ')}
+      </ul>
+    `
+    card.innerHTML = html
+    this.mainContainer.appendChild(card)
   }
 }
